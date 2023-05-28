800f3f6b502fecdb4585d621539d338410e76e98
Marty Lauterbach <LauterbachMK@protonmail.com>
2023-05-28 16:16:18 +0200

    start

--- maze_visualiser.py
+++ maze_visualiser.py
@@ -1,7 +1,8 @@
 """
-code that visualizes the maze
+code that visualises the maze
 
 visualization:
+
             1.1.5 1.2.5 1.3.5 1.4.5 1.5.5
           1.1.4 1.2.4 1.3.4 1.4.4 1.5.4
         1.1.3 1.2.3 1.3.3 1.4.3 1.5.3
@@ -14,6 +15,7 @@ visualization:
       2.1.2 2.2.2 2.3.2 2.4.2 2.5.2
     2.1.1 2.2.1 2.3.1 2.4.1 2.5.1
 
+            3.1.5 3.2.5 3.3.5 3.4.5 3.5.5
           3.1.4 3.2.4 3.3.4 3.4.4 3.5.5
         3.1.3 3.2.3 3.3.3 3.4.3 3.5.3
       3.1.2 3.2.2 3.3.2 3.4.2 3.5.2
@@ -34,6 +36,8 @@ visualization:
 the cubes' wall being checked currently is orange, if it is checked positive for position-change,
 it goes green, and the mouse position is updated to it.
 if it is False for position change, it goes to the system standard again and checks the next one.
+
+
 """
 
 # dict of name maze with dict inside the cubes with 6 keys with the names 1-6 with bool values
---
+++ text.assets.py
@@ -0,0 +1,44 @@
+mouse_pointing_east = (
+    ""
+    ""
+    ""
+)
+mouse_pointing_west = (
+    ""
+    ""
+    ""
+)
+mouse_pointing_forward = (
+    ""
+    ""
+    ""
+)
+mouse_pointing_backwards = (
+    ""
+    ""
+    ""
+)
+mouse_pointing_upwards = (
+    ""
+    ""
+    ""
+)
+mouse_pointing_downwards = (
+    ""
+    ""
+    ""
+)
+
+mouse_art_win = (
+    '''
+                          _   _      Space-Operodent to Bridge
+                         (q\_/p)     Package secured, requesting beam up  
+                          /. .\         __      
+                   ,__   =\_t_/=      .'o O'-.
+                      )   /   \      / O o_.-`|   _   _
+                     (   ((   ))    /O_.-'  O |  (q\_/p)
+                      \  /\) (/\    | o   o  o|   /. .\.-"""""-.     ___, 
+                 jgs   `-\  Y  /    |o   o O.-`  =\_t_/=     /  `\  (
+                          nn^nn     | O _.-'       )\ ))__ __\   |___)
+                                    '--`          (/-(/`  `nn---'
+                ''')
